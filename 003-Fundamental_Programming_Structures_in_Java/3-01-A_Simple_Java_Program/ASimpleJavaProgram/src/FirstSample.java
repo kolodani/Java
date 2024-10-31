76f568aafd009d87ee98103f3c4bf1924c549e77
kolodani <danielvinzia@gmail.com>
2024-10-31 13:48:35 -0300

    first sample

---
+++ 003-Fundamental_Programming_Structures_in_Java/3-01-A_Simple_Java_Program/ASimpleJavaProgram/src/FirstSample.java
@@ -0,0 +1,26 @@
+/*
+ * 3.1 A simple java program
+ */
+
+public class FirstSample {
+    public static void main(String[] args) {
+        System.out.println("We will not use 'Hello, World!'");
+        // salto de linea se hace con los parentesis vacios
+        System.out.println();
+        System.out.println("despues del salto de linea");
+        // si ponemos print en vez de println no hara el salto de linea
+        System.out.print("Primer system");
+        System.out.print("Segundo y va a quedar todo pegado");
+
+    }
+}
+
+/*
+ *  public class ClassName 
+ *  {
+ *      public static void main(String[] args) 
+ *      {
+ *          Program Statements
+        }
+    } 
+ */
