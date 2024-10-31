d9eaf13496f3a1b66bcb246712f059497cfced74
kolodani <danielvinzia@gmail.com>
2024-10-31 13:49:13 -0300

    comments

---
+++ 003-Fundamental_Programming_Structures_in_Java/3-02-Comments/Comments/src/Comments.java
@@ -0,0 +1,19 @@
+/**
+ * 3.2 Comments
+ * @author kolodani
+ */
+
+ public class Comments {
+    public static void main(String[] args) {
+        System.out.println("We will not use 'Hello, World!'"); //comentario despues del la lina de codigo
+        /*
+         * Bloque
+         * de
+         * comentario
+         * para no tener
+         * que hacer //
+         * todo el tiempo
+         */
+    }
+}
+
