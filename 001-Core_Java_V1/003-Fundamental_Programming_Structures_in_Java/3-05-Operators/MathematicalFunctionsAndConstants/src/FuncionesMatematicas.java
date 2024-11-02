24574294662265627faf397638c4c09d526ad948
kolodani <danielvinzia@gmail.com>
2024-11-02 20:07:32 -0300

    Mathematical Functions and Constants

---
+++ 001-Core_Java_V1/003-Fundamental_Programming_Structures_in_Java/3-05-Operators/MathematicalFunctionsAndConstants/src/FuncionesMatematicas.java
@@ -0,0 +1,27 @@
+
+/**
+ * 3.5.2 Mathematical Functions and Constants
+ * @author kolodani
+ */
+
+public class FuncionesMatematicas {
+
+    public static void main(String[] args) {
+
+        double x = 4;
+        double y = Math.sqrt(x);
+        System.out.println(y);
+        double a = 3.0;
+        y = Math.pow(x, a);
+        System.out.println(y);
+
+        /*
+         * Tambien poseemos funciones como seno, coseno y tangente
+         * ademas de contar con exponente y logaritmo
+         * y no nos olvidemos de PI y del numero de euler E
+         */
+
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+    }
+}
