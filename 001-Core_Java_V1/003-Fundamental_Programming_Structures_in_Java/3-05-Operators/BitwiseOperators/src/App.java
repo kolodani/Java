0423f4253061c037531cb06c78a930a939539820
kolodani <danielvinzia@gmail.com>
2024-11-03 15:25:36 -0300

    bitwise opertors

---
+++ 001-Core_Java_V1/003-Fundamental_Programming_Structures_in_Java/3-05-Operators/BitwiseOperators/src/App.java
@@ -0,0 +1,16 @@
+/**
+ * 3.5.8 Bitwise Operators
+ * 
+ * @author kolodani
+ */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        /*
+         * Operadores para bits son &("and"), |("or") , ^("xor"), ~("not")
+         * hay mas operaciones que pueden ser usadas pero es un tema
+         * un poco mas expecifico y no muy usado en general
+         */
+    }
+}
