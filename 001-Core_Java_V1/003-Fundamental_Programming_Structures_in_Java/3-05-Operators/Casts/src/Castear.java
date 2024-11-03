2a09db60bdadd3f076489377b3993c86a774025e
kolodani <danielvinzia@gmail.com>
2024-11-03 14:22:28 -0300

    Casts

---
+++ 001-Core_Java_V1/003-Fundamental_Programming_Structures_in_Java/3-05-Operators/Casts/src/Castear.java
@@ -0,0 +1,21 @@
+/**
+ * 3.5.4 Casts
+ * @author kolodani
+ */
+
+public class Castear {
+
+    public static void main(String[] args) {
+
+        double x = 9.997;
+        System.out.println(x);
+        int nx = (int) x;
+        System.out.println(nx);
+    }
+    
+    /*
+     * se puede castear un tipo un tipo distinto poniendolo entre parentesis
+     * pero recordar por ejemplo en este caso que habra una perdida
+     * de datos de los mismo
+     */
+}
