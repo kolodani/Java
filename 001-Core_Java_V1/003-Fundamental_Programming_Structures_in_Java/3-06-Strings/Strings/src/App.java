4cfc827d3824ba1fc77069ced3b30e668679950e
kolodani <danielvinzia@gmail.com>
2024-11-03 15:38:27 -0300

    strings

---
+++ 001-Core_Java_V1/003-Fundamental_Programming_Structures_in_Java/3-06-Strings/Strings/src/App.java
@@ -0,0 +1,15 @@
+/**
+ * 3.6.0 Strings
+ * 
+ * @author kolodani
+ */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        String e = ""; // string vacio
+        String greetting = "Hello";
+
+        System.out.println(e + " " + greetting);
+    }
+}
