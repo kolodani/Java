8e98071ec332bfbf99c39189d17d1926ad7bfed9
kolodani <danielvinzia@gmail.com>
2024-11-03 15:45:02 -0300

    substrings

---
+++ 001-Core_Java_V1/003-Fundamental_Programming_Structures_in_Java/3-06-Strings/Substrings/src/App.java
@@ -0,0 +1,20 @@
+/**
+ * 3.6.1 Substrings
+ * 
+ * @author kolodani
+ */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        String greeting = "Hello";
+        String s = greeting.substring(0, 3);
+        System.out.println(s);
+        
+        /*
+         * al ser un objeto la clase String, greetting tiene metodos
+         * de la clase String como lo es substring, que sustrae
+         * parte de la cadena de caracteres
+         */
+    }
+}
