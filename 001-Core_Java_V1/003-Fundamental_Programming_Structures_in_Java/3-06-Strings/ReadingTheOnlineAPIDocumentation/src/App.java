c34c17bc9e6b66b24cb74f6e1f70adcb83945942
kolodani <danielvinzia@gmail.com>
2024-11-03 17:07:24 -0300

    reading the online API documentation

---
+++ 001-Core_Java_V1/003-Fundamental_Programming_Structures_in_Java/3-06-Strings/ReadingTheOnlineAPIDocumentation/src/App.java
@@ -0,0 +1,16 @@
+/**
+ * 3.6.8 Reading the Online API Documentation
+ * 
+ * @author kolodani
+ */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        /*
+         * la documentacion es enorme solo hablando de strings
+         * por lo que se recomienda fuertemente que se va siempre la
+         * documentacion online de la APi y familiarizarse con la misma
+         */
+    }
+}
