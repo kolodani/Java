a07e3d349db05b0a9915e9c2b3c2dd061d49a236
kolodani <danielvinzia@gmail.com>
2024-11-03 16:38:45 -0300

    code points and code units

---
+++ 001-Core_Java_V1/003-Fundamental_Programming_Structures_in_Java/3-06-Strings/CodePointsAndCodeUnits/src/App.java
@@ -0,0 +1,22 @@
+/**
+ * 3.6.5 Empty and Null Strings
+ * 
+ * @author kolodani
+ */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        String hola = "Hello";
+        int n = hola.length();
+        System.out.println(n);
+
+        char first = hola.charAt(0); // "H"
+        char last = hola.charAt(4); // "o"
+
+        /*
+         * Metodos de manejo de caracteres y recorrer un arreglo se
+         * veran mas adelante
+         */
+    }
+}
