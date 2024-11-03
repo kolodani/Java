83b86243783f799decc85d5318d87ac157009e9f
kolodani <danielvinzia@gmail.com>
2024-11-03 15:32:06 -0300

    parenteses and operartor hierarchy

---
+++ 001-Core_Java_V1/003-Fundamental_Programming_Structures_in_Java/3-05-Operators/ParenthesesAndOperatorHierarchy/src/App.java
@@ -0,0 +1,17 @@
+/**
+ * 3.5.9 Parentheses and Operator Hierarchy
+ * 
+ * @author kolodani
+ */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        /*
+         * recordar que la mayoria de operadores operan de izquierda
+         * a derecha pero hay un par que operan alreves y ademas
+         * es importante que sucede cuando estos mismos se combinan
+         * para evitar errores
+         */
+    }
+}
