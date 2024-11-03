10222b52fe0945201a92ea35824b952e3ce8d8a4
kolodani <danielvinzia@gmail.com>
2024-11-03 14:29:56 -0300

    combining assigment with operators

---
+++ 001-Core_Java_V1/003-Fundamental_Programming_Structures_in_Java/3-05-Operators/CombiningAssignmentWithOperators/src/Combinando.java
@@ -0,0 +1,22 @@
+/**
+ * 3.5.5 Combining Assignment with Operators
+ * @author kolodani
+ */
+
+public class Combinando {
+
+    public static void main(String[] args) {
+        int x = 0;
+        int y = 0;
+        x += 4;
+        y = y + 4;
+        System.out.println("Valor de X: " + x);
+        System.out.println("Valor de Y: " + y);
+
+        /*
+         * se puede resumir la operacion de suma y de asignacion en la
+         * combinacion del signo +=
+         * como tambien la resta
+         */
+    }
+}
