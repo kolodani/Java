107773eb200eaed82ef4337b874a61ad094b5300
kolodani <danielvinzia@gmail.com>
2024-11-09 15:56:42 -0300

    multidimensional arrays

---
+++ 001-Core_Java_V1/003-Fundamental_Programming_Structures_in_Java/3-10-Arrays/MultidimensionalArrays/src/App.java
@@ -0,0 +1,11 @@
+/**
+ * 3.10.7 Multidimensional Arrys
+ * 
+ * @author kolodani
+ */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
