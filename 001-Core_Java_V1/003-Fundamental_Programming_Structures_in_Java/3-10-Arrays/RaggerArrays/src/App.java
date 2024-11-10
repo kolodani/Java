ad4b1d7b73894e7e2f89fc3310596c1bd87cd713
kolodani <danielvinzia@gmail.com>
2024-11-10 17:20:08 -0300

    ragger arrays

---
+++ 001-Core_Java_V1/003-Fundamental_Programming_Structures_in_Java/3-10-Arrays/RaggerArrays/src/App.java
@@ -0,0 +1,11 @@
+/**
+ * 3.10.8 Ragger Arrays
+ * 
+ * @author kolodani
+ */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
