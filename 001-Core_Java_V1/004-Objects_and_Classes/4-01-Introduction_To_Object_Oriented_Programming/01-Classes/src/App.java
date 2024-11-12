5ed32f9dbcce0b571fae4f0b4763279e819c4287
kolodani <danielvinzia@gmail.com>
2024-11-11 22:34:57 -0300

    classes

---
+++ 001-Core_Java_V1/004-Objects_and_Classes/4-01-Introduction_To_Object_Oriented_Programming/01-Classes/src/App.java
@@ -0,0 +1,30 @@
+/**
+ * 4.1.1 Classes
+ * 
+ * @author kolodani
+ */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        /*
+         * Las clases son los moldes de los objetos.
+         * cuando creas un objeto, creas una instancia de la
+         * clase.
+         * java cuenta con miles de clases listas para ser
+         * usadas como tambien podes hacer las propias.
+         * 
+         * los objetos creados de la misma clase tienen
+         * distintas instancias de campos y uso de sus metodos
+         * los valores son los estados del objeto y cuando se los
+         * invoca estos pueden cambiar.
+         * 
+         * se debe interactuar con los campos atravez de los metodos
+         * y no directamente
+         * 
+         * ademas podemos crear un clase nueva o modificar alguna
+         * existente extendiendola, esto es herencia y se usa siempre
+         * ya que todas las clases heredan de la super clase Object
+         */
+    }
+}
