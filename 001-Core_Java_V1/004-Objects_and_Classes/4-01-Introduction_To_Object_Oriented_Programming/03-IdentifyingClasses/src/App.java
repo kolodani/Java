a2a4e319fc292f4abfdde59f6d586b6114935854
kolodani <danielvinzia@gmail.com>
2024-11-11 22:35:36 -0300

    identifiying classes

---
+++ 001-Core_Java_V1/004-Objects_and_Classes/4-01-Introduction_To_Object_Oriented_Programming/03-IdentifyingClasses/src/App.java
@@ -0,0 +1,19 @@
+/**
+ * 4.1.3 Identifying Classes
+ * 
+ * @author kolodani
+ */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        /*
+         * una regla simple para identificar una clase es
+         * buscar un sustantivo en el problema, entonces
+         * es una clase. si es un vervo es un metodo.
+         * este metodo no es una ciencia perfecta pero ayuda
+         * a empezar a ver un panorama mejor el problema a
+         * resolver con programacion orientada a objetos 
+         */
+    }
+}
