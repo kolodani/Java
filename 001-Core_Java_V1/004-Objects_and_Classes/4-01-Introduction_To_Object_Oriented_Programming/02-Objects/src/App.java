4b86dc2bfc7a81736be942540d0b7c84fb902c8e
kolodani <danielvinzia@gmail.com>
2024-11-11 22:35:12 -0300

    objects

---
+++ 001-Core_Java_V1/004-Objects_and_Classes/4-01-Introduction_To_Object_Oriented_Programming/02-Objects/src/App.java
@@ -0,0 +1,20 @@
+/**
+ * 4.1.2 Objects
+ * 
+ * @author kolodani
+ */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        /*
+         * tres claves que identifican a un objetos
+         * - comportamiento tiene o que metodos le puedo aplicar
+         * - como reacciona a esos metodos cuando se lo invoca
+         * - que lo identifica o distingue de otros objetos que
+         * tienen esos mismos metodos
+         * 
+         * 
+         */
+    }
+}
