239ad24bd8a2f6306495252c0b4500733da043d3
kolodani <danielvinzia@gmail.com>
2024-11-11 22:36:01 -0300

    relationships between classes

---
+++ 001-Core_Java_V1/004-Objects_and_Classes/4-01-Introduction_To_Object_Oriented_Programming/04-RelationshipsBetweenClasses/src/App.java
@@ -0,0 +1,24 @@
+/**
+ * 4.1.4 Relationships between Classes
+ * 
+ * @author kolodani
+ */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        /*
+         * La relaciones mas comunes entre clases son:
+         * - dependencias
+         * - agregacion
+         * - herencia
+         * 
+         * dependencia es cuando una clases depende de metodos
+         * y de campos de otra y le pide cosas
+         * agragacion es cuando una clase contiene a otra y
+         * la usa para si misma
+         * herencia es cuando una clase hereda de otra metodos
+         * y campos
+         */
+    }
+}
