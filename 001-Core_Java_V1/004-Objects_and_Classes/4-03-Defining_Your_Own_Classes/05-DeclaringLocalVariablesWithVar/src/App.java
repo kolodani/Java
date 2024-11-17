92607103b77bd7a5c0ace3705dc7f3d76409879f
kolodani <danielvinzia@gmail.com>
2024-11-17 18:04:35 -0300

    declaring local variables with var

---
+++ 001-Core_Java_V1/004-Objects_and_Classes/4-03-Defining_Your_Own_Classes/05-DeclaringLocalVariablesWithVar/src/App.java
@@ -0,0 +1,20 @@
+/**
+ * 4.3.5 Declaring Local Variables with var
+ * 
+ * @author kolodani
+ */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        /*
+         * se puede declarar un objeto Employee asi:
+         * Employee harry = new Employee("Harry Hacker", 50000, 1989, 10, 1);
+         * y tambien asi:
+         * var harry = new Employee("Harry Hacker", 50000, 1989, 10, 1);
+         * se puede usar var para los objetos
+         * pero no lo usaremos para tipos numericos
+         */
+
+    }
+}
