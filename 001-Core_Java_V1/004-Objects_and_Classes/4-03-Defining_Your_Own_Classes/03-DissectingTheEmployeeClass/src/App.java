24705a912ccd2e5f508d5d95c012339ba7cfd723
kolodani <danielvinzia@gmail.com>
2024-11-17 17:47:30 -0300

    dissecting the employee class and first steps with constructors

---
+++ 001-Core_Java_V1/004-Objects_and_Classes/4-03-Defining_Your_Own_Classes/03-DissectingTheEmployeeClass/src/App.java
@@ -0,0 +1,31 @@
+/**
+ * 4.3.3 Dissecting the Employee Class
+ * 
+ * @author kolodani
+ */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        /*
+         * en el ejemplo de la clase Employee
+         * tiene un constructor y 4 metodos:
+         * 
+         * public Employee(String n, double s, int year, int month, int day)
+         * 
+         * public String getName()
+         * public double getSalary()
+         * public LocalDate getHireDay()
+         * public void raiseSalary(double byPercent)
+         * 
+         * y los campos son 3:
+         * 
+         * private String name;
+         * private double salary;
+         * private LocalDate hireDay;
+         * 
+         * que son privados para mantener el encapsulamiento y evitar
+         * que se puedan alterar sin usar los metodos de la clase
+         */
+    }
+}
---
+++ 001-Core_Java_V1/004-Objects_and_Classes/4-03-Defining_Your_Own_Classes/04-FirstStepsWithConstructors/src/App.java
@@ -0,0 +1,19 @@
+/**
+ * 4.3.4 First Steps with Constructors
+ * 
+ * @author kolodani
+ */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        /*
+         * constructores:
+         * tienen el mismo nombre que la clase
+         * puede haber mas de un constructor
+         * un constructor puede tener cero, uno o mas parametros
+         * un constructor no retorna un valor
+         * simpre se llama con el operador new
+         */
+    }
+}
