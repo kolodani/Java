55dcdbc7373098cc5797101e5e4535ae3ffbd245
kolodani <danielvinzia@gmail.com>
2024-11-18 16:01:02 -0300

    static fields

---
+++ 001-Core_Java_V1/004-Objects_and_Classes/4-04-Static_Fields_and_Methods/01-StaticFields/src/App.java
@@ -0,0 +1,16 @@
+/**
+ * 4.4.1 Static Fields
+ * 
+ * @author kolodani
+ */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        /*
+         * un campo estatico es un campo que es unico por clase
+         * sirve mucho por ejemplo si queremos hacer un grupo de personas
+         * y a cada una le ponemos un numero identificatorio como id
+         */
+    }
+}
