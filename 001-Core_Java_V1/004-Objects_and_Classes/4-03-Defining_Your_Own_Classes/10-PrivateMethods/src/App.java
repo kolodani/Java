2a5f443f6be7e0ecf91fd245ce7a458de3058110
kolodani <danielvinzia@gmail.com>
2024-11-18 15:08:42 -0300

    private methods

---
+++ 001-Core_Java_V1/004-Objects_and_Classes/4-03-Defining_Your_Own_Classes/10-PrivateMethods/src/App.java
@@ -0,0 +1,15 @@
+/**
+ * 4.3.10 Private Methods
+ * 
+ * @author kolodani
+ */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        /*
+         * los metodos privados son solo para usar dentro de la clase
+         * y no ser usados por fuera de la misma
+         */
+    }
+}
