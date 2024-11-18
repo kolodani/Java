da203688635f5df95b6955b06e21b06e2d0fc9a6
kolodani <danielvinzia@gmail.com>
2024-11-18 16:01:47 -0300

    static methods

---
+++ 001-Core_Java_V1/004-Objects_and_Classes/4-04-Static_Fields_and_Methods/03-StaticMethods/src/App.java
@@ -0,0 +1,16 @@
+/**
+ * 4.4.3 Static Methods
+ * 
+ * @author kolodani
+ */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        /*
+         * los metodos estaticos son para usos especiales
+         * cuando no necesitamos del objeto para uae el un metodo
+         * y para acceder a un campo estatico de la clase
+         */
+    }
+}
