ca23fa98a21693c34eeebbdc795e4ff16ddfed10
kolodani <danielvinzia@gmail.com>
2024-11-18 15:08:16 -0300

    class bases access privileges

---
+++ 001-Core_Java_V1/004-Objects_and_Classes/4-03-Defining_Your_Own_Classes/09-ClassBasedAccessPrivileges/src/App.java
@@ -0,0 +1,15 @@
+/**
+ * 4.3.9 Class-Based Access Privileges
+ * 
+ * @author kolodani
+ */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        /*
+         * un metodo puede acceder a cualquier campo privado de un ofjeto
+         * de esa clase. Esto es Muy Importante
+         */
+    }
+}
