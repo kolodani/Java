89f843bb6a1f10d303f84fbed7944d660b49226a
kolodani <danielvinzia@gmail.com>
2024-11-18 14:37:21 -0300

    implicit and explicit parameters

---
+++ 001-Core_Java_V1/004-Objects_and_Classes/4-03-Defining_Your_Own_Classes/07-ImplicitAndExplicitParameters/src/App.java
@@ -0,0 +1,31 @@
+/**
+ * 4.3.7 Implicit and Explicit Parameters
+ * 
+ * @author kolodani
+ */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        /*
+         * public void raiseSalary(double byPercent)
+         * {
+         * double raise = salary * byPercent / 100;
+         * salary += raise;
+         * }
+         * 
+         * en este metodo hay dos parametros, uno implicito y otro explicito
+         * el implicito es Employee y el explicito es byPercent
+         * por eso muchos programadores prefieren escribirlo de la siguiente
+         * forma:
+         * 
+         * public void raiseSalary(double byPercent)
+         * {
+         * double raise = this.salary * byPercent / 100;
+         * this.salary += raise;
+         * }
+         * 
+         * this referencia a Employee
+         */
+    }
+}
