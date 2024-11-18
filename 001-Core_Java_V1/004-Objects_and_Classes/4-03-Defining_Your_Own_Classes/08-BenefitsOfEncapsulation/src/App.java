93e35dd2aec6231e04c0534d84c6fcf692d8022a
kolodani <danielvinzia@gmail.com>
2024-11-18 15:07:51 -0300

    benefits of encapsulation

---
+++ 001-Core_Java_V1/004-Objects_and_Classes/4-03-Defining_Your_Own_Classes/08-BenefitsOfEncapsulation/src/App.java
@@ -0,0 +1,21 @@
+/**
+ * 4.3.8 Benefits of Encapsulation
+ * 
+ * @author kolodani
+ */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        /*
+         * los beneficios del encapsulamiento se pueden usar creando"
+         * un campo privado de datos
+         * un metodo publico de acceso
+         * un metodo publico de cambio
+         * 
+         * tener cuidado si un objeto devuelve un objeto que se lo puede
+         * cambiar. en estos casos usar la clonacion de un objeto asi
+         * evitamos cambiar un objeto que referenciamos de varios lugares
+         */
+    }
+}
