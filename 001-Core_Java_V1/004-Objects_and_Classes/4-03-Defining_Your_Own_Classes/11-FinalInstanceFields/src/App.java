e4f718b16b90909888f01f87acb467ac89b0c1ef
kolodani <danielvinzia@gmail.com>
2024-11-18 15:09:06 -0300

    final instance fields

---
+++ 001-Core_Java_V1/004-Objects_and_Classes/4-03-Defining_Your_Own_Classes/11-FinalInstanceFields/src/App.java
@@ -0,0 +1,17 @@
+/**
+ * 4.3.11 Final Instance Fields
+ * 
+ * @author kolodani
+ */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        /*
+         * el campo final es un campo que no puede cambiar
+         * solamente recibe el valor que se le da cuando se lo inicializa
+         * el mismo
+         * no existe metodo de Set
+         */
+    }
+}
