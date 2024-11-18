1f17cb2cac2126ce514eb546c814f59eb84764bc
kolodani <danielvinzia@gmail.com>
2024-11-18 16:01:20 -0300

    static constants

---
+++ 001-Core_Java_V1/004-Objects_and_Classes/4-04-Static_Fields_and_Methods/02-StaticConstants/src/App.java
@@ -0,0 +1,17 @@
+/**
+ * 4.4.2 Static Constants
+ * 
+ * @author kolodani
+ */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        /*
+         * no hay en si variables estaticas pero si hay constantes estaticas
+         * ademas son finales para no cambiar, como en math esta la constante
+         * PI
+         */
+        System.out.println(Math.PI);
+    }
+}
